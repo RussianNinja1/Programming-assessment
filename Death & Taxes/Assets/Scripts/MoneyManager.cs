498b75b0c3bdda09b03425832e0b9cb5a0bebeab
RussianNinja <stas.katunin@gmail.com>
2023-06-02 16:31:11 +1000

    Coins can now be collected and are stored in the UI, there is a money manager, some back end stuff still needs to be sorted regarding keeping money between levels refer to the usual vid Made first version prefabs for coin and spikes

--- Death & Taxes/Assets/Scripts/MoneyManager.cs
+++ Death & Taxes/Assets/Scripts/MoneyManager.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoneyManager : MonoBehaviour
 
 {
+    public TMP_Text moneyText;
+    public int currentGold; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //PlayerPrefs.SetInt("CurrentMoney", 0);  // bootleg way to reset remembered gold value Need to delete when finished
+        if (PlayerPrefs.HasKey("CurrentMoney"))
+        {
+            currentGold = PlayerPrefs.GetInt("CurrentMoney");
+        }
+        else
+        {
+            currentGold = 0;
+            PlayerPrefs.SetInt("CurrentMoney", 0);
+        }
+        moneyText.text = ": " + currentGold;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void AddMoney(int goldToAdd)
+    {
+        currentGold += goldToAdd;
+        PlayerPrefs.SetInt("CurrentMoney", currentGold);
+        moneyText.text = ":" + currentGold;
+
+
     }
 }
