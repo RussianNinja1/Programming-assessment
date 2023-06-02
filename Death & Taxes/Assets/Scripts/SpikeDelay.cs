4ac5a1c6151e65685910772bd1804055e365d36f
RussianNinja <stas.katunin@gmail.com>
2023-06-02 15:41:28 +1000

    Spike traps feature working implemented, can adjust delay, needs some refinement.

---
+++ Death & Taxes/Assets/Scripts/SpikeDelay.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpikeDelay : MonoBehaviour
+{
+    public float startDelay; // Amount of seconds to wait before playing animation:
+    [SerializeField] private float animRunTime; // Amount of time to play animation: 10 Frames, one per second.
+    private Animator anim;
+
+    private bool triggered;
+    
+    void Start()
+    {
+       anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!triggered)
+        {
+            StartCoroutine(ActivateSpikes());
+
+        }
+    }
+    private IEnumerator ActivateSpikes()
+    {
+        triggered = true;
+        yield return new WaitForSeconds(startDelay);
+        anim.SetBool("activated", true);
+        yield return new WaitForSeconds(animRunTime);
+        triggered = false;
+        anim.SetBool("activated", false);
+    }
+
+}
