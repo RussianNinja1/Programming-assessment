cb40665784ba0e2bd3dd10c80af02e37210c2354
RussianNinja <stas.katunin@gmail.com>
2023-06-07 15:04:48 +1000

    Rent screen set up, needs polish, but other wise funtional, main manu set up and funtional

--- Death & Taxes/Assets/Scripts/PlayerHealthManager.cs
+++ Death & Taxes/Assets/Scripts/PlayerHealthManager.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
 {
-    public int playerMaxHealth;
-    public int playerCurrentHealth;
+    public int playerMaxHealth; //total max health
+    public int playerCurrentHealth; //current health 
 
     // Start is called before the first frame update
     void Start()
     {
-        playerCurrentHealth = playerMaxHealth;
+        playerCurrentHealth = playerMaxHealth;  //sets current health to max health (aka full health on start)
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerCurrentHealth <=0)
+        if(playerCurrentHealth <=0) // if player health is less then or = to 0 deactivate the player object
         {
             gameObject.SetActive(false);
         }
     }
-    public void HurtPlayer(int damageToGive)
+    public void HurtPlayer(int damageToGive)  //public funtions to that gets called on other script, with arguments that you can set to do different damage if needed
     {
         playerCurrentHealth -= damageToGive;
     }
-    public void SetMaxHealth()
+    public void SetMaxHealth()  // public function to call if player ever incounter a healing item, or we need to reset health to full for any reason.
     {
         playerCurrentHealth = playerMaxHealth;
     }
---
+++ Death & Taxes/Assets/Scripts/ShiftTimer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ShiftTimer : MonoBehaviour
+{
+    //public string nextScene; //the next scene i want to load.
+    float currentTime = 0f; 
+    public float startingTime = 10f;
+    [SerializeField] TMP_Text timerText; //addresses the text component in the the timer
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime; //set current time to starting time ( amount of time to complete level).
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime; // decreases time by 1 each second
+        timerText.text = currentTime.ToString("0"); //writes time to Timer text
+
+        if (currentTime <=0)  // loads next scene
+        {
+            currentTime = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+       
+    }
+}
