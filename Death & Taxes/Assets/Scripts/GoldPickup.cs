bb6ab1b040099cef9761f1f8cd303bbb22486e1c
RussianNinja <stas.katunin@gmail.com>
2023-06-22 13:46:19 +1000

    level 2 done, needs play testing. added sound to coin pick up

--- Death & Taxes/Assets/Scripts/GoldPickup.cs
+++ Death & Taxes/Assets/Scripts/GoldPickup.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GoldPickup : MonoBehaviour
 {
     public int value;
     public MoneyManager theMM;
+    public AudioSource coinSound;
+    public float waitTime;
+    SpriteRenderer spriteRend;
+    Collider2D colid;
 
     // Start is called before the first frame update
     void Start()
     {
         theMM = FindObjectOfType<MoneyManager>();
+        spriteRend = GetComponent<SpriteRenderer>();
+        colid = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -22,8 +29,17 @@ public class GoldPickup : MonoBehaviour
     {
         if (other.gameObject.name == "Player")
         {
+            StartCoroutine(CoinSounds());
             theMM.AddMoney(value);
-            Destroy(gameObject);
         }
     }
+    private IEnumerator CoinSounds()
+    {
+        coinSound.Play();
+        colid.enabled = false;
+        spriteRend.enabled = false;
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+    
 }
