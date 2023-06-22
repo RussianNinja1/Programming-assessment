3407c848f73c685bfae2774efa6971db318f74dc
RussianNinja <stas.katunin@gmail.com>
2023-06-23 00:57:52 +1000

    same as below

--- Death & Taxes/Assets/Scripts/Soundeffect.cs
+++ Death & Taxes/Assets/Scripts/Soundeffect.cs
@@ -3,18 +3,6 @@ using UnityEngine;
 public class Soundeffect : MonoBehaviour
 {
     public AudioSource spikes;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void SpikeSound()
     {
         spikes.Play();
